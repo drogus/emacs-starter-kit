c338b20a9e2804a93ed1a07cfa78ce745e11fb62
Piotr Sarnacki <drogus@gmail.com>
2011-03-01 12:50:21 -0800

    automatic changes after install

--- elpa/ruby-mode-1.1/ruby-mode-autoloads.el
+++ elpa/ruby-mode-1.1/ruby-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 
 
-;;;### (autoloads (ruby-mode) "ruby-mode" "ruby-mode.el" (19780 30550))
+;;;### (autoloads (ruby-mode) "ruby-mode" "ruby-mode.el" (19821 22014))
 ;;; Generated autoloads from ruby-mode.el
 
 (autoload 'ruby-mode "ruby-mode" "\
@@ -31,7 +31,7 @@ The variable ruby-indent-level controls the amount of indentation.
 
 ;;;***
 
-;;;### (autoloads nil nil ("ruby-mode-pkg.el") (19780 30550 810783))
+;;;### (autoloads nil nil ("ruby-mode-pkg.el") (19821 22015 146170))
 
 ;;;***
 
