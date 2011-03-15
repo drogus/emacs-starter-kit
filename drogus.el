c318b57f10eef41a24702d1c7e5aba4b5b466442
Piotr Sarnacki <drogus@gmail.com>
2011-03-15 14:01:42 +0100

    Map C-w to backward kill word, new binding for kill region and new bindings for M-x

--- drogus.el
+++ drogus.el
@@ -23,5 +23,12 @@
 (require 'feature-mode)
 (add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
 
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
 
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+
+(defalias 'qrr 'query-replace-regexp)
 
